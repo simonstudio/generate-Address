c93abf0e891a3dd7089a2bda72a5d6c057b80df7
unknown <simon72217@gmail.com>
2022-09-29 02:12:43 +0700

    first commit

--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,5 @@
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/simonstudio/genaddress.git
+# git remote add origin https://github.com/simonstudio/genaddress.git
 git push -u origin main
